f8c56e2814346348e08bf87a06a6dc1e0b5bf507
shinoshinoshino <sh.goto1@gmail.com>
2019-03-16 18:13:35 +0900

    shinodog

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @profiles = Profile.all
+    @profiles = current_user.profile
   end
 
   def new
