30eb9e7f6d379cae02fc9ff6ff57f50965d35072
shinoshinoshino <sh.goto1@gmail.com>
2019-03-17 10:31:00 +0900

    shinodog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_17_004334) do
+ActiveRecord::Schema.define(version: 2019_03_17_012930) do
 
   create_table "friends", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2019_03_17_004334) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "birthweight"
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
