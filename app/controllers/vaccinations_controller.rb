6af3d53e7b8aba0187ceb03863f58bfa69d1c598
shinoshinoshino <sh.goto1@gmail.com>
2019-03-21 09:35:21 +0900

    shinodog

--- app/controllers/vaccinations_controller.rb
+++ app/controllers/vaccinations_controller.rb
@@ -2,37 +2,37 @@ class VaccinationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @vaccinations = Vaccination.all.order(created_at: 'desc')
+    @vaccinations = current_user.vaccinations.all.order(created_at: 'desc')
   end
 
   def new
   end
 
   def create
-    @vaccination = Vaccination.new(vaccination_params)
+    @vaccination = current_user.vaccinations.new(vaccination_params)
     @vaccination.save
     redirect_to vaccinations_path
   end
 
   def destroy
-    @vaccination = Vaccination.find(params[:id])
+    @vaccination = current_user.vaccinations.find(params[:id])
     @vaccination.destroy
     redirect_to vaccinations_path
   end
 
   def edit
-    @vaccination = Vaccination.find(params[:id])
+    @vaccination = current_user.vaccinations.find(params[:id])
   end
 
   def update
-    @vaccination = Vaccination.find(params[:id])
+    @vaccination = current_user.vaccinations.find(params[:id])
     @vaccination.update(vaccination_params)
     redirect_to vaccinations_path
   end
 
   private
     def vaccination_params
-      params.require(:vaccination).permit(:date, :rabies, :vaccine, :filaria, :flea)
+      params.require(:vaccination).permit(:date, :rabies, :vaccine, :filaria, :flea, :user_id)
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_17_012930) do
+ActiveRecord::Schema.define(version: 2019_03_21_002919) do
 
   create_table "friends", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 2019_03_17_012930) do
     t.date "vaccine"
     t.date "filaria"
     t.date "flea"
+    t.integer "user_id"
   end
 
   add_foreign_key "medicines", "profiles"
