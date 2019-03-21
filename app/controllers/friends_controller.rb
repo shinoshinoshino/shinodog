fed3fe8894729223fca7b2c5e6dae05e63d90b43
shinoshinoshino <sh.goto1@gmail.com>
2019-03-21 09:49:31 +0900

    shinodog

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,37 +1,37 @@
 class FriendsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
-    @friends = Friend.all
+    @friends = current_user.friends.all
   end
 
   def new
   end
 
   def create
-    @friend = Friend.new(friend_params)
+    @friend = urrent_user.friends.new(friend_params)
     @friend.save
     redirect_to friends_path
   end
 
   def destroy
-    @friend = Friend.find(params[:id])
+    @friend = urrent_user.friends.find(params[:id])
     @friend.destroy
     redirect_to friends_path
   end
 
   def edit
-    @friend = Friend.find(params[:id])
+    @friend = urrent_user.friends.find(params[:id])
   end
 
   def update
-    @friend = Friend.find(params[:id])
+    @friend = urrent_user.friends.find(params[:id])
     @friend.update(friend_params)
     redirect_to friends_path
   end
 
   private
     def friend_params
-      params.require(:friend).permit(:name, :breed, :gender, :owner, :coat, :feature)
+      params.require(:friend).permit(:name, :breed, :gender, :owner, :coat, :feature, :user_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_002919) do
+ActiveRecord::Schema.define(version: 2019_03_21_004307) do
 
   create_table "friends", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_03_21_002919) do
     t.text "feature"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "medicines", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
