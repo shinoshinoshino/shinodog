e0ecf6f15929a8ab46d1d99c1e2086f8eb39a79e
shinoshinoshino <sh.goto1@gmail.com>
2019-03-21 11:55:00 +0900

    shinodog

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,7 @@
 class Profile < ApplicationRecord
+  belongs_to :user
   has_many :posts, dependent: :destroy
   has_many :medicines, dependent: :destroy
-  belongs_to :user
+  has_many :users, through: :posts
+
 end
